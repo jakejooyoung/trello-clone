3171d8b320efacfb5f38dab790a6b4c21ac89030
Jake Kim <jake@npgains.com>
2018-04-02 15:54:59 -0700

    Configure db with .env

--- api/config/database.js
+++ api/config/database.js
@@ -1,23 +1,29 @@
-{
+const dotenv = require('dotenv').config();
+const Sequelize = require("sequelize");
+
+module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": "root",
+    "database": "trello-devdb",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "operatorsAliases": Sequelize.Op
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "trello-testdb",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "operatorsAliases": Sequelize.Op
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "operatorsAliases": Sequelize.Op
   }
 }
