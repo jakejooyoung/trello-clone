7a56b7a8462340d9c232b3393c3cb7318a4ba673
Jake Kim <jake@npgains.com>
2018-04-02 11:50:46 -0700

    Add babel-preset-react to devdependency and add .babelrc file to configure babel-loader presets

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+// In webpack.config.js
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const UglifyJsPlugin = require("uglify-js-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+const path = require("path");
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + "/client/index.html",
+  filename: "index.html",
+  inject: "body",
+  links: [
+    "https://fonts.googleapis.com/css?family=Roboto",
+  ]
+});
+const config = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "static"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+          test: /\.jsx$/,
+          loader: "babel-loader", 
+      },
+      {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+      },
+      { 
+        test: [/\.scss$/, /\.css$/],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      }, 
+      {
+        test: /\.json$/, 
+        use: "json-loader",  
+      },      
+      {
+        test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/, loader: "file-loader"
+      }
+    ]
+  },
+  performance: {
+    hints: "warning", // enum
+    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000, // int (in bytes)
+    assetFilter: function(assetFilename) { 
+      // Function predicate that provides asset filenames
+      return assetFilename.endsWith(".css") || assetFilename.endsWith(".js");
+    }
+  },
+  devtool: "source-map", // enum
+  // enhance debugging by adding meta info for the browser devtools
+  // source-map most detailed at the expense of build speed.
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new ExtractTextPlugin({
+      filename: "app.css",
+      allChunks: true,
+    })
+  ]
+};
+module.exports = config;
+
