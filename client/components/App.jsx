97f553b9272b50e6e37b0d4288a194ea33242818
Jake Kim <jake@npgains.com>
2018-04-02 11:14:05 -0700

    Update App.jsx, index.html, index.js files for Hello World\!

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="app">
+				Hello World!
+			</div>
+		);
+	}
+}
+
+
---
+++ client/index.js
@@ -0,0 +1,10 @@
+/* 
+    ./client/index.js
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+const app = <App />;
+
+ReactDOM.render(app, document.getElementById('app-container'));
