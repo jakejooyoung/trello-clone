92a319f70bb2b9170c3d9b66eceb92e41c4ed80b
Jake Kim <jake@npgains.com>
2018-04-03 02:45:10 -0700

    Lint webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,75 @@
 // In webpack.config.js
-const webpack = require("webpack")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const UglifyJsPlugin = require("uglify-js-plugin")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+// const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglify-js-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
-const path = require("path");
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + "/src/index.html",
-  filename: "index.html",
-  inject: "body",
+  template: path.resolve(__dirname, 'src/index.html'),
+  filename: 'index.html',
+  inject: 'body',
   links: [
-    "https://fonts.googleapis.com/css?family=Roboto",
-  ]
+    'https://fonts.googleapis.com/css?family=Roboto',
+  ],
 });
 const config = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-          test: /\.jsx$/,
-          loader: "babel-loader", 
+        test: /\.jsx$/,
+        loader: 'babel-loader',
       },
       {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
       },
-      { 
+      {
         test: [/\.scss$/, /\.css$/],
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
-      }, 
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
       {
-        test: /\.json$/, 
-        use: "json-loader",  
-      },      
+        test: /\.json$/,
+        use: 'json-loader',
+      },
       {
-        test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/, loader: "file-loader"
-      }
-    ]
+        test: /\.(eot|woff|woff2|svg|ttf)([?]?.*)$/, loader: 'file-loader',
+      },
+    ],
   },
   performance: {
-    hints: "warning", // enum
+    hints: 'warning', // enum
     maxAssetSize: 200000, // int (in bytes),
     maxEntrypointSize: 400000, // int (in bytes)
-    assetFilter: function(assetFilename) { 
-      // Function predicate that provides asset filenames
-      return assetFilename.endsWith(".css") || assetFilename.endsWith(".js");
-    }
+    assetFilter: assetFilename => (
+      assetFilename.endsWith('.css') ||
+      assetFilename.endsWith('.js')
+    ),
   },
-  devtool: "source-map", // enum
+  devtool: 'source-map', // enum
   // enhance debugging by adding meta info for the browser devtools
   // source-map most detailed at the expense of build speed.
   plugins: [
     HTMLWebpackPluginConfig,
     new UglifyJsPlugin({
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
     new ExtractTextPlugin({
-      filename: "app.css",
+      filename: 'app.css',
       allChunks: true,
-    })
-  ]
+    }),
+  ],
 };
 module.exports = config;
 
