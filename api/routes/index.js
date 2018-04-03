8b3afd2ebe1557b78e322bcf03df3a31882cc923
Jake Kim <jake@npgains.com>
2018-04-02 22:35:34 -0700

    Rename cards.js to tasks.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,12 @@
 import users from './users';
 import boards from './boards';
 import columns from './columns';
-import cards from './cards';
+import tasks from './tasks';
 
 module.exports = app => {
 	// Add routes here.
 	app.use('/users', users);
 	app.use('/boards', boards);
 	app.use('/columns', columns);
-	app.use('/cards', cards);
+	app.use('/tasks', tasks);
 }
--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const models = require('../data/models');
+
+import Sequelize from 'sequelize';
+const { Op } = Sequelize;
+
+router.get('/', function (req, res,next) {
+  // Should be checking auth, if not signed in redirect to signup.
+  const asy = async () => {
+    const tasks = await models.Task.findAll();
+    if (tasks) {
+      res.send(tasks);
+    }
+  };
+  asy().catch(next);
+})
 
 router.route('/')
   // route specific middleware
   .all(function(req,res,next){
-    console.log("You've reached /cards route.");
+    console.log("You've reached /tasks route.");
     next();
   })
   .get(function(req, res, next) {
-    res.send("Hello from the /cards route");
+    res.send("Hello from the /tasks route");
   })
   .put(function(req, res, next) {
     next(new Error('not implemented'));
